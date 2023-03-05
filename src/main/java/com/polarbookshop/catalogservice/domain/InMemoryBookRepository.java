80bef439c5c72989a7fb13c14c1a050feb8b472b
Omar Shabaan <omar.plus.077@gmail.com>
2023-03-05 12:39:35 +0200

    returning the new added value instead of the previous one

--- src/main/java/com/polarbookshop/catalogservice/domain/InMemoryBookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/InMemoryBookRepository.java
@@ -9,30 +9,31 @@ import java.util.Optional;
 @Repository
 public class InMemoryBookRepository implements BookRepository {
 
-    private final Map<String, Book> books = new HashMap<>();
+  private final Map<String, Book> books = new HashMap<>();
 
-    @Override
-    public Iterable<Book> findAll() {
-      return books.values();
-    }
+  @Override
+  public Iterable<Book> findAll() {
+    return books.values();
+  }
 
-    @Override
-    public Optional<Book> findByIsbn(String isbn) {
-      return Optional.ofNullable(books.get(isbn));
-    }
+  @Override
+  public Optional<Book> findByIsbn(String isbn) {
+    return Optional.ofNullable(books.get(isbn));
+  }
 
-    @Override
-    public boolean existsByIsbn(String isbn) {
-        return books.containsKey(isbn);
-    }
+  @Override
+  public boolean existsByIsbn(String isbn) {
+    return books.containsKey(isbn);
+  }
 
-    @Override
-    public Book save(Book book) {
-      return books.put(book.isbn(), book);
-    }
+  @Override
+  public Book save(Book book) {
+    books.put(book.isbn(), book);
+    return books.get(book.isbn());
+  }
 
-    @Override
-    public void deleteByIsbn(String isbn) {
+  @Override
+  public void deleteByIsbn(String isbn) {
       books.remove(isbn);
     }
 }
