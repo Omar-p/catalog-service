a2d08ae4a8993e10ff9b001aaca7d77052d0b332
Omar Shabaan <omar.plus.077@gmail.com>
2023-03-05 12:43:21 +0200

    using WebTestClient in integration test for post request

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient webTestClient;
 
+
+	@Test
+	void whenPostBook_thenBookIsAdded() {
+		Book book = new Book("9780321146533", "A valid title", "A valid author", 10.0);
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(book)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class)
+				.isEqualTo(book);
+	}
 }
