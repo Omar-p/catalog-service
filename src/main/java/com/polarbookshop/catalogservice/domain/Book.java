d7a296353852c9ae28db44fa174e76e83408fd92
Omar Shabaan <omar.plus.077@gmail.com>
2023-03-05 12:36:56 +0200

    adding validation annotation and test it

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,8 +1,22 @@
 package com.polarbookshop.catalogservice.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 public record Book(
+   @NotBlank(message = "The book ISBN must be defined")
+   @Pattern(
+       regexp = "^(97(8|9))?[0-9]{9}(X|[0-9])$",
+       message = "The book ISBN must be a valid ISBN-10 or ISBN-13"
+   )
    String isbn,
+   @NotBlank(message = "The book title must be defined")
    String title,
+   @NotBlank(message = "The book author must be defined")
    String author,
+   @NotNull(message = "The book price must be defined")
+   @Positive(message = "The book price must be greater than zero")
    Double price) {
 }
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,38 @@
+package com.polarbookshop.catalogservice.domain;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class BookValidationTests {
+
+  private static Validator validator;
+
+  @BeforeAll
+  static void setUp() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  void whenAllFieldsAreValid_thenNoConstraintViolations() {
+    Book book = new Book("9780321146533", "A valid title", "A valid author", 10.0);
+    BDDAssertions.assertThat(validator.validate(book)).isEmpty();
+  }
+
+  @Test
+  void whenIsbnIsBlank_thenConstraintViolation() {
+    Book book = new Book(" ", "A valid title", "A valid author", 10.0);
+    BDDAssertions.assertThat(validator.validate(book)).hasSize(2);
+  }
+
+  @Test
+  void whenIsbnIsInvalid_thenConstraintViolation() {
+    Book book = new Book("978032114653", "A valid title", "A valid author", 10.0);
+    BDDAssertions.assertThat(validator.validate(book)).hasSize(1);
+  }
+
+}
