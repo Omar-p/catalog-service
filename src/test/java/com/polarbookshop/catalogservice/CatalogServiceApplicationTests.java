5489470cdbf653748990ba03e76b05a2c5c83133
Omar Shabaan <omar.plus.077@gmail.com>
2023-03-05 23:45:32 +0200

    don't fail in tests when config server is down

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -4,11 +4,16 @@ import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@TestPropertySource(properties = {
+		"spring.cloud.config.fail-fast=false",
+		"spring.cloud.config.request-connect-timeout=0",
+})
 class CatalogServiceApplicationTests {
 
 	@Autowired
